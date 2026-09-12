f4a1c81d8a3bdbbb2670ef2720dfa2d8fd2ac4b0
Thiagoo-Franca <tfranca@ufba.br>
2026-09-12 12:49:11 -0300

    feat: hero slides and texts

--- components/projetos/post.tsx
+++ components/projetos/post.tsx
@@ -1,3 +1,5 @@
+import { ArrowIcon } from "../ui/icons";
+
 export default function Post() {
   return (
     <div
@@ -7,19 +9,7 @@ export default function Post() {
       }}
     >
       <div className="bg-secondary w-6 h-6 md:w-10 md:h-10 rounded-full flex items-center justify-center absolute top-4 right-4 opacity-0 group-hover:opacity-100 transition-all duration-300">
-        <svg
-          className="w-3 h-3 text-white"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2.5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-          />
-        </svg>
+        <ArrowIcon />
       </div>
       <div className="absolute bottom-4 left-4 md:opacity-0 group-hover:opacity-100 transition-all duration-300">
         <h2 className="text-xl font-bold text-secondary">Nome do Projeto</h2>
