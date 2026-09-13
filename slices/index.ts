0f8ca062393ae171829fb7ceaae2456e4a80f316
Thiagoo-Franca <tfranca@ufba.br>
2026-09-13 17:00:46 -0300

    Update Prismic models

--- slices/index.ts
+++ slices/index.ts
@@ -3,11 +3,13 @@
 import CallToAction from "./CallToAction";
 import Footer from "./Footer";
 import Hero from "./Hero";
+import Projetos from "./Projetos";
 import QuemSomos from "./QuemSomos";
 
 export const components = {
-  call_to_action: CallToAction,
-  footer: Footer,
-  hero: Hero,
-  quem_somos: QuemSomos,
+	call_to_action: CallToAction,
+	footer: Footer,
+	hero: Hero,
+	projetos: Projetos,
+	quem_somos: QuemSomos
 };
