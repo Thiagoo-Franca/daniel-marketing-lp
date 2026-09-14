0da26d20e152874704e850884b8cd45f832ad473
Thiagoo-Franca <tfranca@ufba.br>
2026-09-14 15:34:54 -0300

    feat: design ajustes

--- slices/CallToAction/index.tsx
+++ slices/CallToAction/index.tsx
@@ -28,15 +28,15 @@ const CallToAction: FC<CallToActionProps> = ({ slice }) => {
         <div className="absolute inset-0 bg-black/70" />
 
         <Reveal>
-          <div className="relative z-10 mx-auto flex max-w-2xl flex-col items-center justify-center gap-4 px-4 text-center">
+          <div className="relative z-10 mx-auto flex max-w-7xl flex-col items-center justify-center gap-4 px-2 text-center">
             <Reveal delay={0.15}>
-              <h2 className="mb-2 md:mb-4 max-w-md text-5xl font-bold text-white leading-tight md:text-7xl">
+              <h2 className="mb-2 md:mb-4 max-w-xl text-5xl font-bold text-white leading-tight md:text-7xl">
                 {slice.primary.frase_chamada}{" "}
                 <span className="text-secondary">{slice.primary.red_text}</span>
               </h2>
             </Reveal>
             <Reveal delay={0.3}>
-              <p className="text-gray-400 mb-8 max-w-md md:max-w-xl">
+              <p className="w-full text-gray-400 mb-8 max-w-md md:max-w-2xl">
                 {slice.primary.paragraph}
               </p>
             </Reveal>
