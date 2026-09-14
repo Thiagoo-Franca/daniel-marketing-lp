5e9913ea29dd5f16ad5774bc955119f985efb37f
Thiagoo-Franca <tfranca@ufba.br>
2026-09-13 23:04:51 -0300

    fix: post tamanho

--- components/projetos/post.tsx
+++ components/projetos/post.tsx
@@ -15,14 +15,16 @@ export default function Post({ onClick, projeto_data }: PostProps) {
       }}
       onClick={onClick}
     >
-      <div className="bg-secondary w-6 h-6 md:w-10 md:h-10 rounded-full flex items-center justify-center absolute top-4 right-4 opacity-0 group-hover:opacity-100 transition-all duration-300">
+      <div className="absolute inset-0 bg-black/0 group-hover:bg-black/60 transition-all duration-300 rounded-lg" />
+
+      <div className="bg-secondary w-6 h-6 md:w-10 md:h-10 rounded-full flex items-center justify-center absolute top-4 right-4 opacity-0 group-hover:opacity-100 transition-all duration-300 z-10">
         <ArrowIcon />
       </div>
-      <div className="absolute bottom-4 left-4 md:opacity-0 group-hover:opacity-100 transition-all duration-300">
+      <div className="absolute bottom-4 left-4 md:opacity-0 group-hover:opacity-100 transition-all duration-300 z-10">
         <h2 className="text-xl font-bold text-secondary">
           {projeto_data?.data.titulo}
         </h2>
-        <p className="text-gray-400 text-sm text-white">
+        <p className="text-gray-400 text-sm text-white hidden md:block">
           {projeto_data?.data.descricao}
         </p>
       </div>
